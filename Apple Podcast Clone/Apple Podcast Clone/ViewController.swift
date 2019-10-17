5d6cf7bbb5242a2a7c11f6a6a836d925c1756920
rutaks <samrutakayile@awesomity.rw>
2019-10-17 12:41:46 +0200

    Initial Setup

---
+++ Apple Podcast Clone/Apple Podcast Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apple Podcast Clone
+//
+//  Created by Awesome Mac on 17/10/2019.
+//  Copyright © 2019 Rutaks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
