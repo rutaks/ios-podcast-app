e66a1bcd39a573feaefc3cfe5c6a1dcb730c89ea
rutaks <samrutakayile@awesomity.rw>
2019-10-19 07:02:49 +0200

    Add Podcast Model

---
+++ Apple Podcast Clone/Apple Podcast Clone/Controllers/PodcastSearchController.swift
@@ -0,0 +1,15 @@
+//
+//  PodcastSearchController.swift
+//  Apple Podcast Clone
+//
+//  Created by Awesome Mac on 19/10/2019.
+//  Copyright © 2019 Rutaks. All rights reserved.
+//
+
+import UIKit
+
+class PodcastSearchController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Apple Podcast Clone/Apple Podcast Clone/Models/Podcast.swift
@@ -0,0 +1,14 @@
+//
+//  Podcast.swift
+//  Apple Podcast Clone
+//
+//  Created by Awesome Mac on 19/10/2019.
+//  Copyright © 2019 Rutaks. All rights reserved.
+//
+
+import Foundation
+
+struct Podcast {
+    let name: String
+    let artistName: String
+}
