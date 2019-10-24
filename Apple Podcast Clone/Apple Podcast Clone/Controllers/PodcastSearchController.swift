119dce0daecfbd1581c7bbe1209de8ea21aed29e
rutaks <samrutakayile@awesomity.rw>
2019-10-24 16:19:23 +0200

    Added Load Image on View Podcasts & 'Search For Podcast Hint'

--- Apple Podcast Clone/Apple Podcast Clone/Controllers/PodcastSearchController.swift
+++ Apple Podcast Clone/Apple Podcast Clone/Controllers/PodcastSearchController.swift
@@ -13,7 +13,7 @@ import SVProgressHUD
 class PodcastSearchController: UITableViewController, UISearchBarDelegate {
     
     var podcasts = [Podcast]()
-    
+    var isSearching: Bool = false
     let cellId = "cellId"
     
     let searchController = UISearchController(searchResultsController: nil)
@@ -33,11 +33,27 @@ class PodcastSearchController: UITableViewController, UISearchBarDelegate {
     }
     
     fileprivate func setupTableView() {
+        tableView.tableFooterView = UIView()
         let nib = UINib(nibName: "PodcastCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: cellId)
     }
     
     //MARK:-Table View Setup
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if(!isSearching){
+            let label = UILabel()
+            label.text = "Please Enter A Search Term"
+            label.textAlignment = .center
+            label.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+            return label
+        }
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return isSearching ? 0 : 250
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return podcasts.count
@@ -60,9 +76,11 @@ class PodcastSearchController: UITableViewController, UISearchBarDelegate {
     //TODO: Implement Api Request On Text Change
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchText.count != 0 {
+            isSearching = false
             SVProgressHUD.show()
         }
         APIService.shared.fetchPodcasts(searchText: searchText) { (podcasts) in
+            self.isSearching=true
             self.podcasts = podcasts
             self.tableView.reloadData()
             SVProgressHUD.dismiss()
